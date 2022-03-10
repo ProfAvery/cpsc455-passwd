453afe8ab9478e2fbe4068539d6e7063983bff27
Kenytt Avery <kavery@fullerton.edu>
2022-03-09 16:38:38 -0800

    Initial upload

---
+++ api.js
@@ -0,0 +1,45 @@
+const os = require('os')
+const crypto = require('crypto')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3').verbose()
+const bcrypt = require('bcrypt')
+
+const PORT = 8080
+
+const app = express()
+app.use(bodyParser.json())
+
+const db = new sqlite3.Database('./users.db')
+
+app.post('/register', (req, res) => {
+  const username = req.body.username
+  const passwd = req.body.password
+
+  const stmt = db.prepare(`
+    INSERT INTO users(username, password, hashed, salted)
+    VALUES(?, ?, ?, ?)
+  `)
+
+  stmt.run(username, passwd, passwd, passwd)
+  stmt.finalize()
+
+  res.status(201)
+  res.json({ username: username })
+})
+
+app.listen(PORT, () => {
+  db.serialize(() => {
+    db.run(`
+      CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY,
+        username VARCHAR,
+        password VARCHAR,
+        hashed VARCHAR,
+        salted VARCHAR
+      )
+    `)
+  })
+  console.log(`Server running at http://${os.hostname()}:${PORT}/`)
+})
